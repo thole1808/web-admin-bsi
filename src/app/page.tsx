96480251f1c0bbefff36a6b533cb14e4744b931c
Achmad Agus <achmad.agus@lawangsewu.com>
2024-10-23 01:04:02 +0700

    add template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import DefaultLayout from "@/components/Layouts/DefaultLayout";
 
 export const metadata: Metadata = {
   title:
-    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
+    "Web Admin BSI - Template",
   description: "This is Next.js Home for TailAdmin Dashboard Template",
 };
 
