0fdd8bad0c48201e3cbb0f342fc7daf4643fe1ea
Achmad Agus <achmad.agus@lawangsewu.com>
2024-10-25 13:49:28 +0700

    sidebar UI BSI referensi Figma Sentuh

--- src/components/Sidebar/SidebarItem.tsx
+++ src/components/Sidebar/SidebarItem.tsx
@@ -23,47 +23,42 @@ const SidebarItem = ({ item, pageName, setPageName }: any) => {
   const isItemActive = isActive(item);
 
   return (
-    <>
-      <li>
-        <Link
-          href={item.route}
-          onClick={handleClick}
-          className={`${isItemActive ? "bg-graydark dark:bg-meta-4" : ""} group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4`}
-        >
-          {item.icon}
-          {item.label}
-          {item.children && (
-            <svg
-              className={`absolute right-4 top-1/2 -translate-y-1/2 fill-current ${
-                pageName === item.label.toLowerCase() && "rotate-180"
-              }`}
-              width="20"
-              height="20"
-              viewBox="0 0 20 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M4.41107 6.9107C4.73651 6.58527 5.26414 6.58527 5.58958 6.9107L10.0003 11.3214L14.4111 6.91071C14.7365 6.58527 15.2641 6.58527 15.5896 6.91071C15.915 7.23614 15.915 7.76378 15.5896 8.08922L10.5896 13.0892C10.2641 13.4147 9.73651 13.4147 9.41107 13.0892L4.41107 8.08922C4.08563 7.76378 4.08563 7.23614 4.41107 6.9107Z"
-                fill=""
-              />
-            </svg>
-          )}
-        </Link>
+    <li className="relative">
+      {/* Garis hover di sebelah kiri */}
+      <div
+        className={`absolute left-0 top-0 bottom-0 w-1 bg-green-500 transition-all duration-300 ease-in-out ${
+          isItemActive ? "opacity-100" : "opacity-0"
+        }`} style={{ color: "#00BFB2" }}
+      />
 
+      <Link
+        href={item.route}
+        onClick={handleClick}
+        className={`${
+          isItemActive ? "bg-transparan dark:bg-meta-4 with-border" : ""
+        } group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4`}  
+
+        style={{ color: "#00BFB2" }} // Menambahkan warna teks hijau sesuai permintaan
+      >
+        {item.icon}
+        {item.label}
         {item.children && (
-          <div
-            className={`translate transform overflow-hidden ${
-              pageName !== item.label.toLowerCase() && "hidden"
-            }`}
-          >
-            <SidebarDropdown item={item.children} />
-          </div>
+          <svg
+            // ... (svg code)
+          />
         )}
-      </li>
-    </>
+      </Link>
+
+      {item.children && (
+        <div
+          className={`translate transform overflow-hidden ${
+            pageName !== item.label.toLowerCase() && "hidden"
+          }`}
+        >
+          <SidebarDropdown item={item.children} />
+        </div>
+      )}
+    </li>
   );
 };
 
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -15,7 +15,7 @@ interface SidebarProps {
 
 const menuGroups = [
   {
-    name: "MENU",
+    // name: "MENU",
     menuItems: [
       {
         icon: (
@@ -152,7 +152,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   return (
     <ClickOutside onClick={() => setSidebarOpen(false)}>
       <aside
-        className={`fixed left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:translate-x-0 ${
+        className={`fixed left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-white duration-300 ease-linear dark:bg-boxdark lg:translate-x-0 ${
           sidebarOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
@@ -195,18 +195,19 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
           <nav className="mt-5 px-4 py-4 lg:mt-9 lg:px-6">
             {menuGroups.map((group, groupIndex) => (
               <div key={groupIndex}>
-                <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
+                <h3 className="mb-4 ml-4 text-sm font-semibold" style={{ color: "#00BFB2" }}>
                   {group.name}
                 </h3>
 
                 <ul className="mb-6 flex flex-col gap-1.5">
                   {group.menuItems.map((menuItem, menuIndex) => (
-                    <SidebarItem
-                      key={menuIndex}
-                      item={menuItem}
-                      pageName={pageName}
-                      setPageName={setPageName}
-                    />
+                    <li key={menuIndex} style={{ color: "#00BFB2" }}>
+                      <SidebarItem
+                        item={menuItem}
+                        pageName={pageName}
+                        setPageName={setPageName}
+                      />
+                    </li>
                   ))}
                 </ul>
               </div>
@@ -214,6 +215,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
           </nav>
           {/* <!-- Sidebar Menu --> */}
         </div>
+
       </aside>
     </ClickOutside>
   );
