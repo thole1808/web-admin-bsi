878ecb2052d4790218b1a3bb07cb94fff37b900e
Achmad Agus <achmad.agus@lawangsewu.com>
2024-10-29 01:41:46 +0700

    update UI Dshboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,25 +1,148 @@
 
 "use client";
 import dynamic from "next/dynamic";
-import React from "react";
+import React, { useState, useRef, useEffect } from 'react';
 import GrafikPengunjung from "../Charts/GrafikPengunjung";
 import TodayVisitors from '../TodayVisitor/TodayVisitors';
-import CardDataStats from "../CardDataStats";
-import { FaUsers } from "react-icons/fa";
 import StatsCard from '../StatsCard/StatsCard';
-import TodayVisitorsTable from "../TopBranchesTable/TopBranchesTable";
 import TopBranchesTable from "../TopBranchesTable/TopBranchesTable";
 
-
 const MapOne = dynamic(() => import("@/components/Maps/MapOne"), { ssr: false });
-const ChartThree = dynamic(() => import("@/components/Charts/ChartThree"), { ssr: false });
-// const ChartFour = dynamic(() => import("@/components/Charts/ChartFour"), { ssr: false });
+
+
 
 const Dashboard: React.FC = () => {
+
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const dropdownRef = useRef(null);
+
+  // Misalkan ini data pengguna yang diambil dari API atau state
+  const user = {
+    name: 'Admin',
+    avatar: 'https://avatar.iran.liara.run/public/8', // Ganti dengan URL avatar sesungguhnya
+  };
+
+  // Data JSON untuk dropdown, dengan avatar
+  const dropdownData = [
+    { id: 1, name: 'Admin', avatar: 'https://avatar.iran.liara.run/public/8' },
+    { id: 2, name: 'User', avatar: 'https://avatar.iran.liara.run/public/8' },
+    { id: 3, name: 'Guest', avatar: 'https://avatar.iran.liara.run/public/8' },
+    { id: 4, name: 'Moderator', avatar: 'https://avatar.iran.liara.run/public/8' },
+  ];
+
+  // Close dropdown when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  // Handle item click
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+    setDropdownOpen(false);
+  };
+
   return (
     <>
-      <div className="min-h-screen bg-gray-100 p-4">
+      <div className="min-h-screen bg-gray-100 p-5">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+          <div className="col-span-1 md:col-span-3 space-y-6 py-5 w-full">
+            <div className="flex space-x-4">
+              <div className="relative w-full">
+                <input
+                  type="text"
+                  placeholder="Cari disini"
+                  className="w-full px-4 py-2 border rounded-xl pr-10"
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 50 50"
+                  width="24px"
+                  height="24px"
+                  className="absolute right-3 top-3 text-gray-500"
+                >
+                  <path
+                    d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"
+                  />
+                </svg>
+              </div>
+
+              <button className="flex items-center bg-white space-x-2 px-4 py-2 border rounded-lg">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3 4h18l-7 8v6.5l-4 2v-8.5l-7-8z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div className="col-span-1 py-5 space-y-6">
+            <div className="relative inline-block text-left w-full" ref={dropdownRef}>
+              <button
+                className="flex justify-between items-center bg-white px-6 py-1 w-full rounded-lg shadow-lg focus:outline-none"
+                onClick={() => setDropdownOpen(!dropdownOpen)}
+              >
+                <div className="flex items-center">
+                  <img
+                    src={user.avatar} // Menggunakan URL avatar pengguna
+                    alt="User Avatar"
+                    className="w-8 h-8 rounded-full mr-1"
+                  />
+                  <span className="font-medium text-lg px-2">{user.name}</span>
+                </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  className="w-6 h-6 text-gray-600"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+                </svg>
+              </button>
+
+              {dropdownOpen && (
+                <div className="absolute right-0 mt-2 w-full bg-white rounded-lg shadow-lg py-2 z-10">
+                  {dropdownData.map((item) => (
+                    <a
+                      key={item.id}
+                      href="#"
+                      className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100"
+                      onClick={() => handleItemClick(item)}
+                    >
+                      <img
+                        src={item.avatar} // Menampilkan avatar untuk setiap item
+                        alt={`${item.name} Avatar`}
+                        className="w-8 h-8 rounded-full mr-2"
+                      />
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+        
           <div className="col-span-1 md:col-span-3 space-y-6">
             {/* <div className="grid grid-cols-3 md:grid-cols-3 gap-6"> */}
             <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
@@ -40,7 +163,7 @@ const Dashboard: React.FC = () => {
               <StatsCard
                 icon={
                   <svg width="114" height="114" viewBox="0 0 114 114" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path opacity="0.21" fill-rule="evenodd"  d="M57 114C88.4802 114 114 88.4802 114 57C114 25.5198 88.4802 0 57 0C25.5197 0 -3.05176e-05 25.5198 -3.05176e-05 57C-3.05176e-05 88.4802 25.5197 114 57 114Z" fill="#00BFB2" />
+                    <path opacity="0.21" fill-rule="evenodd" d="M57 114C88.4802 114 114 88.4802 114 57C114 25.5198 88.4802 0 57 0C25.5197 0 -3.05176e-05 25.5198 -3.05176e-05 57C-3.05176e-05 88.4802 25.5197 114 57 114Z" fill="#00BFB2" />
                     <path d="M44.0103 51.4504H35.9884C34.3406 51.4504 33 52.9846 33 54.8688V76.583C33 78.4669 34.3406 79.9999 35.9884 79.9999H44.0103C45.658 79.9999 46.9982 78.4669 46.9982 76.5831V54.8688C46.9984 52.9846 45.6581 51.4504 44.0103 51.4504Z" fill="#00A39D" />
                     <path d="M59.6483 34.0001C57.1208 34.0001 55.0646 36.0302 55.0646 38.5267C55.0646 38.6952 55.0748 38.8637 55.0941 39.0339C55.0638 39.1828 55.0605 39.3351 55.0933 39.4826C55.9684 43.4485 54.1444 47.9525 52.9784 50.283C52.8775 50.4837 52.7794 50.6791 52.6852 50.8565C52.5088 51.0581 52.2706 51.1943 52.0015 51.2436H51.6575C51.6079 51.2436 51.5295 51.2485 51.4693 51.2558C49.5562 51.3627 48.0777 52.8978 48.0777 54.7795V76.4647C48.0777 78.4137 49.6836 79.9998 51.6575 79.9998H76.4132C76.5224 79.9998 76.6273 79.9864 76.7266 79.9609C79.0453 79.7196 80.8259 77.7916 80.8259 75.4717C80.8259 74.2241 80.3117 73.0918 79.4775 72.2724C80.4102 71.4429 81 70.2406 81 68.9054C81 67.6137 80.4496 66.4476 79.5686 65.6214C80.4495 64.7967 81 63.629 81 62.3375C81 61.0459 80.4496 59.8798 79.5686 59.0551C80.4495 58.2288 81 57.0627 81 55.771C81 53.2746 78.9436 51.243 76.415 51.243L65.8263 51.2446C68.7872 42.1527 63.5298 36.0561 63.2886 35.7855C63.2837 35.7808 63.2788 35.7759 63.2739 35.7677C62.4075 34.6596 61.0623 34.0001 59.6483 34.0001Z" fill="#00A39D" />
                   </svg>
@@ -75,7 +198,7 @@ const Dashboard: React.FC = () => {
               <TopBranchesTable />
             </div>
           </div>
-          
+
         </div>
       </div>
     </>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,8 +10,8 @@ const Header = (props: {
   setSidebarOpen: (arg0: boolean) => void;
 }) => {
   return (
-    <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
-      <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
+    <header className="sticky top-0 z-999 flex w-full bg-transparant drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
+      {/* <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11"> */}
         <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
           {/* <!-- Hamburger Toggle BTN --> */}
           <button
@@ -69,7 +69,7 @@ const Header = (props: {
         <div className="hidden sm:block">
           <form action="https://formbold.com/s/unique_form_id" method="POST">
             <div className="relative">
-              <button className="absolute left-0 top-1/2 -translate-y-1/2">
+              {/* <button className="absolute left-0 top-1/2 -translate-y-1/2">
                 <svg
                   className="fill-body hover:fill-primary dark:fill-bodydark dark:hover:fill-primary"
                   width="20"
@@ -91,13 +91,13 @@ const Header = (props: {
                     fill=""
                   />
                 </svg>
-              </button>
+              </button> */}
 
-              <input
+              {/* <input
                 type="text"
                 placeholder="Type to search..."
                 className="w-full bg-transparent pl-9 pr-4 font-medium focus:outline-none xl:w-125"
-              />
+              /> */}
             </div>
           </form>
         </div>
@@ -109,19 +109,19 @@ const Header = (props: {
             {/* <!-- Dark Mode Toggler --> */}
 
             {/* <!-- Notification Menu Area --> */}
-            <DropdownNotification />
+            {/* <DropdownNotification /> */}
             {/* <!-- Notification Menu Area --> */}
 
             {/* <!-- Chat Notification Area --> */}
-            <DropdownMessage />
+            {/* <DropdownMessage /> */}
             {/* <!-- Chat Notification Area --> */}
           </ul>
 
           {/* <!-- User Area --> */}
-          <DropdownUser />
+          {/* <DropdownUser /> */}
           {/* <!-- User Area --> */}
         </div>
-      </div>
+      {/* </div> */}
     </header>
   );
 };
