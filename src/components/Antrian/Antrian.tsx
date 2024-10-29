f7bfd2eda434d58ec339148a3359fe53a32cf9af
Achmad Agus <achmad.agus@lawangsewu.com>
2024-10-29 17:14:46 +0700

    perbaikan haeder table antrian

--- src/components/Antrian/Antrian.tsx
+++ src/components/Antrian/Antrian.tsx
@@ -255,7 +255,31 @@ const AntrianTable: React.FC = () => {
 
   return (
     <div>
-      <div className="flex justify-between mb-8 mt-30">
+      <div className="col-span-1 md:col-span-5 space-y-6 py-1 w-full mb-13">
+        <div className="flex space-x-1">
+          <div className="relative w-full">
+            <input
+              type="text"
+              placeholder="Cari disini"
+              className="w-full px-4 py-2 border rounded-xl pr-10"
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 50 50"
+              width="24px"
+              height="24px"
+              className="absolute right-3 top-3 text-gray-500"
+            >
+              <path
+                d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="flex justify-between mb-8 mt-0">
         <div className="flex items-center justify-between space-x-4 bg-white shadow-md rounded-lg p-2">
           {/* Filter Icon */}
           <div className="flex items-center space-x-2">
