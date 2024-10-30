4da908c089cf2eb2ef78c0714a10fc695cb4cf1e
Achmad Agus <achmad.agus@lawangsewu.com>
2024-10-31 04:04:32 +0700

    perbaikan table cabang new design figma

--- src/components/Cabang/Cabang.tsx
+++ src/components/Cabang/Cabang.tsx
@@ -418,7 +418,6 @@ const CabangTable: React.FC = () => {
         },
     ], []);
 
-
     useEffect(() => {
         const handleClickOutside = (event: MouseEvent) => {
             if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
@@ -442,13 +441,8 @@ const CabangTable: React.FC = () => {
         },
         {
             name: "Nama Cabang",
+            selector: (row: Cabang) => row.namaCabang,
             sortable: true,
-            cell: (row: Cabang) => (
-                <div className="flex items-center">
-                    <input type="checkbox" className="mr-2" />
-                    <span>{row.namaCabang}</span>
-                </div>
-            ),
             omit: !visibleColumns.namaCabang,
         },
         {
@@ -500,35 +494,18 @@ const CabangTable: React.FC = () => {
         );
     }, [search, dataCabang]);
 
-    // Handle item click
-    const handleItemClick = (item: DropdownItem) => {
-        setSelectedItem(item);
-        setDropdownOpen(false);
-    };
-
     return (
         <div>
-            <div className="flex mb-10 justify-between items-center  py-5 w-full">
-                {/* Search Input */}
+            <div className="flex mb-10 justify-between items-center py-5 w-full">
                 <div className="relative w-1/2">
                     <input
                         type="text"
                         placeholder="Cari disini"
                         className="w-full px-4 py-2 border rounded-xl pr-10"
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
                     />
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 50 50"
-                        width="24px"
-                        height="24px"
-                        className="absolute right-3 top-3 text-gray-500"
-                    >
-                        <path
-                            d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"
-                        />
-                    </svg>
                 </div>
-
                 {/* User Dropdown */}
                 <div className="relative inline-block text-left w-1/5" ref={dropdownRef}>
                     <button
