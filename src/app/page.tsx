1933d085e145d63bacc1a1ed8673aef66e269cf9
Achmad Agus <achmad.agus@lawangsewu.com>
2024-12-23 11:38:16 +0700

    add cek session page first login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,22 @@
+"use client"; // Pastikan hanya di sini
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import Dashboard from "@/components/Dashboard/Dashboard";
-import { Metadata } from "next";
-import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import Auth from "@/components/Login/Auth";
 
-export const metadata: Metadata = {
-  title:
-    "Web Admin BSI - Template",
-  description: "Web Admin BSI",
-};
-
 export default function Home() {
+  const { data: session, status } = useSession(); 
+  const router = useRouter(); 
+
+  if (session) {
+    router.push("/dashboard");
+    return null; // Tidak merender apa pun sampai pengalihan selesai
+  }
+
   return (
-    
-    <Auth/>
+    <Auth />
   );
 }
 
-//  GOOD
-// "use client";
 
-// import { useSession } from "next-auth/react"; // Import useSession untuk mengecek sesi
-// import { useEffect } from "react";
-// import { useRouter } from "next/navigation";
-// import DefaultLayout from "@/components/Layouts/DefaultLayout";
-// import Auth from "@/components/Login/Auth";
-
-// export default function Home() {
-//   const { data: session, status } = useSession(); // Ambil data session
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     // Jika sesi ada, arahkan ke dashboard
-//     if (session) {
-//       router.push("/dashboard");
-//     }
-//   }, [session, router]);
-
-//   if (status === "loading") {
-//     return <p>Loading...</p>; // Tampilkan loading saat status masih loading
-//   }
-
-//   if (!session) {
-//     return (
-//       <DefaultLayout>
-//         <Auth /> {/* Menampilkan halaman login jika tidak ada sesi */}
-//       </DefaultLayout>
-//     );
-//   }
-
-//   return null; // Jangan render apapun jika ada sesi, karena kita sudah mengarahkan ke dashboard
-// }
-  
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -89,7 +89,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
             <Image
               width={176}
               height={32}
-              src={"/images/logo/logo-bsi.png"}
+              src={"/images/logo/logo-bsi.sdsd"}
               alt="Logo"
               priority
             />
