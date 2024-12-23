ba13ed9012a679794712d523dcf518b08cccf866
Achmad Agus <achmad.agus@lawangsewu.com>
2024-12-23 11:38:39 +0700

    add cek session page first login

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -89,7 +89,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
             <Image
               width={176}
               height={32}
-              src={"/images/logo/logo-bsi.sdsd"}
+              src={"/images/logo/logo-bsi.png"}
               alt="Logo"
               priority
             />
