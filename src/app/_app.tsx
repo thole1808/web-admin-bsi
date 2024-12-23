3b431373cdaaf4952d387f3f734bdd4198b94ebd
Achmad Agus <achmad.agus@lawangsewu.com>
2024-12-24 03:01:04 +0700

    enable header mobile responsive

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,14 +1,3 @@
-// import { SessionProvider } from "next-auth/react";  // Import SessionProvider
-
-// export default function App({ Component, pageProps }) {
-//   return (
-//     <SessionProvider session={pageProps.session}>
-//       <Component {...pageProps} />
-//     </SessionProvider>
-//   );
-// }
-
-
 import { SessionProvider } from "next-auth/react";  // Import SessionProvider
 import React from "react";
 import { AppProps } from "next/app";
