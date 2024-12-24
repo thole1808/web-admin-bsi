5ca2e8ae8bb6b771b3702b857b96ca65010f790b
Achmad Agus <achmad.agus@lawangsewu.com>
2024-12-24 16:06:26 +0700

    perbaikan styles Master Checklist

--- src/app/master/checklist/page.tsx
+++ src/app/master/checklist/page.tsx
@@ -4,19 +4,18 @@ import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import Checklist from "@/components/Master/Checklist/Checklist";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
-// import NoSSR from "@/components/no-ssr";
 
 export const metadata: Metadata = {
     title:
         "Web Admin BSI - Template",
     description: "Web Admin BSI",
 };
-// const NoSSR = dynamic(() => import('@/components/no-ssr'), { ssr: false });
+
 const FormElementsPage = () => {
     return (
         <DefaultLayout>
             <div className="flex flex-col gap-10">
-                {/* <Breadcrumb pageName="Antrian"/> */}
+                {/* <Breadcrumb pageName="Checklist"/> */}
                 {/* # */}
                 <Checklist />
             </div>
