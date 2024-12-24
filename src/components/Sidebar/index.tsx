48967bfadc89a44ccd6381ff39ac6dcb1bba50e4
Achmad Agus <achmad.agus@lawangsewu.com>
2024-12-24 14:59:05 +0700

    perbaikan styles approval mmatrix

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -100,7 +100,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
             <Image
               width={176}
               height={32}
-              src={"/images/logo/logo-bsi.pnsdsg"}
+              src={"/images/logo/logo-bsi.png"}
               alt="Logo"
               priority
             />
