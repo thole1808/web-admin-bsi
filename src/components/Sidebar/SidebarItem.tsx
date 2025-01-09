bcf3a70fd816515c441491bb7954ec42c04db38e
Achmad Agus <achmad.agus@lawangsewu.com>
2025-01-09 14:59:52 +0700

    add menu Settings Content

--- src/components/Sidebar/SidebarItem.tsx
+++ src/components/Sidebar/SidebarItem.tsx
@@ -3,6 +3,8 @@ import { usePathname } from "next/navigation";
 import SidebarDropdown from "@/components/Sidebar/SidebarDropdown";
 import { FaHome, FaUsers, FaDatabase, FaCalendarCheck, FaChartBar, FaRegBuilding, } from 'react-icons/fa';
 import Link from 'next/link'
+import SettingsIcon from "../Icon/SettingsIcon";
+import { FaWrench } from "react-icons/fa6";
 
 const IconMap: { [key: string]: JSX.Element } = {
   DashboardIcon: <FaHome className="w-4.5 h-4.5" />,
@@ -12,6 +14,7 @@ const IconMap: { [key: string]: JSX.Element } = {
   AntrianIcon: <FaCalendarCheck className="w-4.5 h-4.5" />,
   CabangIcon: <FaRegBuilding className="w-4.5 h-4.5" />,
   ReportingIcon: <FaChartBar className="w-4.5 h-4.5" />,
+  SettingsIcon: <FaWrench className="w-4.5 h-4.5" />,
 };
 
 const SidebarItem = ({ item }: any) => {
